8a61e92717a6ea3a3142aefeb12da04c55190060
Web-Developer-Rakib <rakibodesk7@gmail.com>
2023-08-13 21:40:19 +0600

    Basic css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="flex justify-center mt-5">
+      <div className="text-5xl font-bold text-slate-700">
+        <h1>We'd love to help</h1>
+        <h1 className="ml-[150px] mt-2">Solve your</h1>
+        <h1 className="ml-[80px] mt-2">Outreach Problems</h1>
+      </div>
+      <p className="w-56 text-[10px] mt-12 ml-[-80px] text-slate-500">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+        distinctio perferendis molestiae rem alias, numquam illum laboriosam
+        iste,
+      </p>
+    </div>
+  );
 }
 
 export default App;
